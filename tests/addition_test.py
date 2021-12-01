d28d03b41ac5724265146c0738b199d23623fba4
Junior Toussaint <juniorro98@outlook.com>
2021-12-01 15:59:28 -0500

    more changes

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -8,9 +8,11 @@ def test_calculation_addition():
     data = get_csv_data("addition.csv")
     for row in data:
         mynumbers = tuple(value)
+        print(mynumbers)
         #Act
         addition = Addition(mynumbers)
         #Assert
         csvwriter = get_log_file_write("result.log")
-        csvwriter.writerow([row])
-        assert addition.get_result() == 9.0
+        csvwriter.writerow(row, addition.get_result())
+        # assert addition.get_result() == 9.0
+        assert 0.9 == 9.0
