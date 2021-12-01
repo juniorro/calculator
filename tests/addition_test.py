f390efd8c19d303f33e7af4e2549706f86b739ac
Junior Toussaint <juniorro98@outlook.com>
2021-11-30 23:43:49 -0500

    added more files

--- tests/addition_test.py
+++ tests/addition_test.py
@@ -1,11 +1,16 @@
 """ Testing Addition """
 from calc.calculations.addition import Addition
+from file_util import get_csv_data, get_log_file_write
 
 def test_calculation_addition():
     """ Testing static method for addition """
     #Arrange
-    mynumbers = (1.0, 8.0)
-    #Act
-    addition = Addition(mynumbers)
-    #Assert
-    assert addition.get_result() == 9.0
+    data = get_csv_data("addition.csv")
+    for row in data:
+        mynumbers = tuple(value)
+        #Act
+        addition = Addition(mynumbers)
+        #Assert
+        csvwriter = get_log_file_write("result.log")
+        csvwriter.writerow([row])
+        assert addition.get_result() == 9.0
--- tests/file_util.py
+++ tests/file_util.py
@@ -1,6 +1,12 @@
-import pandas as pd 
+import pandas as pd
+import csv
+    
+def get_csv_data(filename):
+    data = pd.read_csv("data/testfiles/" + filename)
+    return data.values
 
-
-data = pd.read_csv("data/testfiles/addition.csv")
-
-print(data)
+def get_log_file_write(filename):
+    with open("data/log/" + filename, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["first", "second", "result"])
+        return csv.writer(csvfile)
